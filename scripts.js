7c00a27cb8231db247484f702b1c5b068ef74a11
SoulKaike <ksoaresdemoura@gmail.com>
2024-03-18 08:30:49 -0300

    Formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
-var botao = document.getElementById("botao");
-botao.addEventListener("click", alertar);
+const botao = document.getElementById("botao");
+botao.addEventListener("click", alertar,false);
+
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
 
 function alertar(){
-      alert("O botão foi clicado!!!");
+      //alert("O nome inserido foi" + nome.value);
 
-}
+      saida.innerText  = "Nome:" + nome.value +
+            "\n Email:" + email.value +
+            "\n Telefone:" +Telefone.value +
+            "\n CEP:" + cep.value +
+            "\n Logradouro:" + Logradouro.value +
+            "\n Número:" + .value +
+            "\n Complemento:" + complemento.value +                        ;
+            "\n Bairro:" + Bairro.value +
+            "\n Cidade:" + Cidade.value +
+            "\n Estado:" + email.value +
+
+      }
